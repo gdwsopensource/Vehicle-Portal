8245033ff14420a28bcba4fe23113f688c2ce864
Shinenadeng <shinena_deng@163.com>
2017-07-11 14:32:56 +0800

    add controller

--- src/main/java/com/gdws/vehicle/PortalApplication.java
+++ src/main/java/com/gdws/vehicle/PortalApplication.java
@@ -2,8 +2,10 @@ package com.gdws.vehicle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class PortalApplication {
 
 	public static void main(String[] args) {
