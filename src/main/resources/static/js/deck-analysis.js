cd709a7690957bc9632da721f206b3c3cc829791
jifengdehao <2975830779@qq.com>
2017-07-11 18:20:03 +0800

    18:20

---
+++ src/main/resources/static/js/deck-analysis.js
@@ -0,0 +1,65 @@
+(function($){
+	$('#search_btn').on('click',function(){
+		var plate_no=$('#plate_no').val();
+		if(plate_no !== ""){
+			 $.ajax({  
+			        type: "get",  
+			        async: false,  
+			        url: "searchFakePlateCar?plateNo="+plate_no,  
+			        success: function(data){
+			        	console.log(data);
+			        	if(data.code === 200){
+			        		if(data.data === "null"){
+			        			$('#result').html('没有查询到结果~');
+			        		}else{
+			        			$('#result').html(readTableFrame(data.data));
+			        		}
+			        	}
+			        },
+			        error:function(err){
+			        	console.log("请求出错----"+err);
+			        }
+			 });
+		}else{
+			 $.ajax({  
+			        type: "get",  
+			        async: false,  
+			        url: "searchFakePlateCarALL",  
+			        success: function(data){
+			        	console.log(data);
+			        	if(data.code === 200){
+			        		if(data.data === "null"){
+			        			$('#result').html('没有查询到结果~');
+			        		}else{
+			        			$('#result').html(readTableFrame(data.data));
+			        		}
+			        	}
+			        },
+			        error:function(err){
+			        	console.log("请求出错----"+err);
+			        }
+			 });
+		}
+		return false;
+	});
+	function readTableFrame(data){
+		var html="";
+		html+="<table class='table'>";
+		html+="<thead><tr><th>车牌号码</th><th>日期</th><th>是否为假套牌车</th></tr></thead>";
+		html+="<tbody>";
+		for(var i=0;i<data.length;i++){
+			html+="<tr>";
+			html+="<td>"+data[i].plate_no+"</td>";
+			html+="<td>"+data[i].cross_date+"</td>";
+			if(data[i].fake_plate_type ==="0"){
+				html+="<td>是</td>";
+			}else if(data[i].night_active ==="1"){
+				html+="<td>否</td>";
+			}	
+			html+="</tr>";
+		}
+		html+="</tbody>";
+		html+="</table>";
+		return html;
+	}
+})(jQuery);
